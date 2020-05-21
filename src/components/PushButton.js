e8cb579e1d544818bc15d09837ad76a9515e64b9
Agafonov Andrew <pentri4@gmail.com>
2020-05-21 19:42:44 +0700

    Add JSDoc to component

--- src/components/PushButton.js
+++ src/components/PushButton.js
@@ -1,27 +1,21 @@
-import React,{useState,useEffect} from "react";
+import React from "react";
+
 /**
- * @PushButton - кнопка
- * buttonText - текст внутри кнопки
- * onClick - функция, срабатывающая при нажатии на кнопку
- * key - ключ
- * btnCls - класс для стилей кнопки
+ * PushButton - кнопка
+ *
+ * @constructor
+ * @param {Object} props - объект с конфигурацией кнопки
+ * @param {String} props.buttonText - текст внутри кнопки
+ * @param {Function} props.onClick - функция, срабатывающая при нажатии на кнопку
+ * @param {String} props.key - ключ
+ * @param {String} props.btnCls - класс для стилей кнопки
  **/
 const PushButton = props => {
         const buttonText = props.buttonText || '',
-        onClick = props.onClick || {},
-        btnCls = props.btnCls || 'default-pushbutton';
-        let startPosition;
-        if(window.innerHeight>420)
-                startPosition=window.innerHeight-60
-        else
-                startPosition=360
-        const [position,setPosition] = useState({top: startPosition+'px'})
-        useEffect(() => {
-                if(window.innerHeight>420)
-                        setPosition({top: window.innerHeight-60+'px'})
-        },[window.innerHeight])
+        onClick = props.onClick || (()=>{}),
+        btnCls = `default-pushbutton ${props.btnCls}` || 'default-pushbutton';
         return (
-                <div className = {btnCls} onClick = {onClick()} style={position}>
+                <div className = {btnCls} onClick = {onClick}>
                     {buttonText}
                 </div>
         );
