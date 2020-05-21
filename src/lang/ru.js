3c66ee5b442ed4359af18c812af0b6bc26071b18
Agafonov Andrew <pentri4@gmail.com>
2020-05-21 19:42:18 +0700

    Add month enums

--- src/lang/ru.js
+++ src/lang/ru.js
@@ -10,6 +10,36 @@ export const ru = {
         }
     },
 
+    monthText:{
+        jan: 'Январь',
+        feb: 'Февраль',
+        mar: 'Март',
+        apr: 'Апрель',
+        may: 'Май',
+        jun: 'Июнь',
+        jul: 'Июль',
+        aug: 'Авугст',
+        sep: 'Сентябрь',
+        oct: 'Октябрь',
+        nov: 'Ноябрь',
+        dec: 'Декабрь'
+    },
+
+    monthDate: {
+        1: 'jan',
+        2: 'feb',
+        3: 'mar',
+        4: 'apr',
+        5: 'may',
+        6: 'jun',
+        7: 'jul',
+        8: 'aug',
+        9: 'sep',
+        10: 'oct',
+        11: 'nov',
+        12: 'dec'
+    },
+
     default:{
         textfield: {
             fieldName: 'Поле'
