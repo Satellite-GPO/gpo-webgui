41725ee4f08307498b1d3e9c96e36288a8abe818
Agafonov Andrew <pentri4@gmail.com>
2020-05-21 19:28:40 +0700

    Add JSDoc to component

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,12 +1,27 @@
 import React from "react";
 
+
+/**
+* TextField - базовый компонент ввода
+*
+* @constructor
+* @param {Object} props - объект с конфигурацией компонента
+* @param {String} props.fieldName - заголовок(label) поля
+* @param {String} props.inputCls - css классы стилей для поля ввода
+* @param {String} props.fieldStyle - css классы стилей для обёртки поля ввода //TODO возможно убрать
+* @param {Boolean} props.readOnly - доступно ли поле ввода только для чтения
+* @param {String} props.type - тип поля ввода
+* @param {String/Number} props.fieldValue - значение поля
+* @param {Function} props.onChange - функцияя выполняемая при изменении поля
+* */
 const TextField = props => {
         const fieldName = `${props.fieldName}:` || '',
-        inputCls = `default-inputCls ${props.inputCls}` || 'default-inputCls',
-        fieldStyle = props.fieldStyle || {},
-        readOnly = props.readOnly || false,
-        type = props.type || 'text',
-        fieldValue = props.fieldValue || '';
+            inputCls = `default-inputCls ${props.inputCls}` || 'default-inputCls',
+            fieldStyle = props.fieldStyle || {},
+            readOnly = props.readOnly || false,
+            type = props.type || 'text',
+            fieldValue = props.fieldValue || '',
+            onChange = props.onChange || (() => {})();
         return (
                 <div className='default-textfield'>
                 <label className='default-textfield-label'>
@@ -16,7 +31,9 @@ const TextField = props => {
                         className={inputCls}
                         type={type}
                         value={fieldValue}
-                        readOnly={readOnly}/>
+                        readOnly={readOnly}
+                        onChange={onChange}
+                        />
                 </label>
             </div>
         );
