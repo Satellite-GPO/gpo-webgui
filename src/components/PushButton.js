00242c18902a9f0ccaeb3a44a5bf96b79ab5a520
Agafonov Andrew <pentri4@gmail.com>
2020-06-07 21:41:03 +0700

    Add disabled state feature to PushButton component

--- src/components/PushButton.js
+++ src/components/PushButton.js
@@ -7,13 +7,20 @@ import React from "react";
  * @param {Object} props - объект с конфигурацией кнопки
  * @param {String} props.buttonText - текст внутри кнопки
  * @param {Function} props.onClick - функция, срабатывающая при нажатии на кнопку
+ * @param {Boolean} props.isDisabled - проверка на отключение кнопки
  * @param {String} props.key - ключ
  * @param {String} props.btnCls - класс для стилей кнопки
  **/
 const PushButton = props => {
-        const buttonText = props.buttonText || '',
-        onClick = props.onClick || (()=>{}),
-        btnCls = props.btnCls ? `default-pushbutton ${props.btnCls}` : 'default-pushbutton';
+        let buttonText = props.buttonText || '',
+            disabled = props.isDisabled || false,
+            btnCls = props.btnCls ? `default-pushbutton ${props.btnCls}` : 'default-pushbutton',
+            onClick = props.onClick || (()=>{});
+        console.log('disabled:', disabled);
+        if(disabled) {
+                onClick = ()=>{};
+                btnCls = `default-pushbutton-disabled ${btnCls}`;
+        }
         return (
                 <div className = {btnCls} onClick = {onClick}>
                     {buttonText}
