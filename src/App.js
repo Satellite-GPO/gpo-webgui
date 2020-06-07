87c0d03503779265d49b5ec114800248342a6375
Agafonov Andrew <pentri4@gmail.com>
2020-06-07 20:47:20 +0700

    Return style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 import './theme/App.css';
 
 import MapContainer from "./components/gis/MapContainer";
@@ -6,15 +6,12 @@ import ControlPanel from "./components/ControlPanel/ControlPanel";
 
 
 function App() {
-    const [style,setStyle] = useState({width: window.innerwidth+'px',height: window.innerHeight+'px'})
-    // useEffect(() => {
-    //         setStyle({
-    //             width: window.innerWidth+'px',
-    //             height: window.innerHeight+'px'
-    //         })
-    // })
+    const style = {
+        height: window.innerHeight+'px'
+    };
+
     return (
-        <div className="App" style={style}>
+        <div id="App" className="App" style={style}>
             <ControlPanel/>
             <MapContainer/>
         </div>
